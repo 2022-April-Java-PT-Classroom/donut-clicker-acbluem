38031c21eb23e7d98b86960681f3e5fb37d4c1bf
acbluem <acbluem@gmail.com>
2022-07-02 14:04:55 -0400

    auto clicker purchase method created and tested

--- donut-maker.js
+++ donut-maker.js
@@ -10,6 +10,22 @@ class DonutMaker {
     this.numOfDonuts += 1;
   }
 
+  buyAutoClicker() {
+    let autoClickerPrice;
+    if (this.numOfDonuts >= 100 && this.numOfAutoClickers == 0) {
+      this.numOfDonuts -= 100;
+      this.numOfAutoClickers += 1;
+      autoClickerPrice *= 1.01;
+    } else {
+      if (this.numOfDonuts < autoClickerPrice) {
+        console.log('Not enough!');
+      } else {
+        this.numOfDonuts -= autoClickerPrice;
+        autoClickerPrice *= 1.01;
+      }
+    }
+  }
+
 }
 
 export default DonutMaker;
--- donut-maker.test.js
+++ donut-maker.test.js
@@ -12,4 +12,21 @@ describe('DonutMaker object', () => {
     const underTest = new DonutMaker(0, 0, 0);
     expect(underTest.numOfDonuts).toEqual(0);
   });
+
+  test('Does donutMaker retrieve auto clicker count', () => {
+    const underTest = new DonutMaker(0, 0, 0);
+    expect(underTest.numOfAutoClickers).toEqual(0);
+  });
+
+  test('Does donutMaker add to auto clicker count', () => {
+    const underTest = new DonutMaker(100, 0, 0);
+    underTest.buyAutoClicker();
+    expect(underTest.numOfAutoClickers).toEqual(1);
+  });
+
+  test('Does donutMaker subtract from donut count', () => {
+    const underTest = new DonutMaker(100, 0, 0);
+    underTest.buyAutoClicker();
+    expect(underTest.numOfDonuts).toEqual(0);
+  })
 });
